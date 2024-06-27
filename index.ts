f0fdc44483928f599c5565cef354db824689179f
nehaayaz579 <nehaayaz48@gmail.com>
2024-06-27 18:29:46 +0500

    This is my word counter project.

---
+++ index.ts
@@ -0,0 +1,12 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const Answer = await inquirer.prompt(
+    {
+        message:"Enter your message to count the words." ,
+        name:"sentence",
+        type:"input",
+    }
+)
+const words = Answer.sentence.trim().split(' ');
+console.log(words);
+console.log(`Your sentence words count is ${words.length}`);
